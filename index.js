b32d8d9b962cd63a03cba96297273a3820303b81
Shelby Trinca <shelby.trinca@gmail.com>
2018-08-02 15:25:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target');
+}
+
+function increaseRankBy(n) {
+  var rankedList = document.querySelectorAll('.ranked-list');
+  for (let i=0; i < rankedList.length; i++) {
+    rankedList[i].innerHTML = parseInt(rankedList[i].innerHTML)+n;
+  }
+}
+
+function deepestChild() {
+  let grandNode = document.querySelector('#grand-node');
+  let nextNode = grandNode.children[0];
+  while (nextNode) {
+    grandNode = nextNode;
+    nextNode = grandNode.children[0];
+  }
+  return grandNode;
+}
